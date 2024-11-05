00b2b3a7a88ceeac38c556ba6acf73573edba3a5
Digi J <fightwithmusicinc@gmail.com>
2024-11-05 01:59:17 -0800

    Create scanner.php

---
+++ basescammer-wordpress-plugin/templates/scanner.php
@@ -0,0 +1,11 @@
+<?php if (!defined('ABSPATH')) exit; ?>
+<div id="basescammer-container">
+    <div id="basescammer-left-panel">
+        <div id="basescammer-loading">Loading tokens...</div>
+        <div id="basescammer-token-list"></div>
+    </div>
+    <div id="basescammer-right-panel">
+        <h2>Select a token to view details</h2>
+        <div id="basescammer-token-info"></div>
+    </div>
+</div>
